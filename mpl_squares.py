ff591b377588f022752b2d25556a464845b4b24e
MowenPan <pan19950817@hotmail.com>
2017-07-27 14:50:00 +0800

    绘制简单折线图

--- mpl_squares.py
+++ mpl_squares.py
@@ -1,12 +1,15 @@
 import matplotlib.pyplot as plt
 
+input_values = [i for i in range(1, 6)]
 squares = [i**2 for i in range(1, 6)]
-plt.plot(squares, linewidth=5)
+plt.plot(input_values, squares, linewidth=5)
 
+# 设置折线图标题,给坐标轴添加标签
 plt.title("Squares Numbers", fontsize=24)
 plt.xlabel("Value", fontsize=14)
 plt.ylabel("Square Of Value", fontsize=14)
 
+# 设置刻度标记的大小
 plt.tick_params(axis='both', labelsize=14)
 
 plt.show()
